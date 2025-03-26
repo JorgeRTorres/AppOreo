d27571fa5666b3010bc226cddeaad0abe0f9da56
JorgeRTorres <jorgetorres.0996@gmail.com>
2025-03-26 18:47:12 -0500

    v5

--- app/src/main/java/com/example/apporeo/screens/LoginScreen.kt
+++ app/src/main/java/com/example/apporeo/screens/LoginScreen.kt
@@ -1,13 +1,17 @@
 package com.example.apporeo.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
@@ -22,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -36,8 +41,25 @@ fun LoginScreen(navController: NavController){
     var errorUsuario = remember { mutableStateOf(false) }
     var errorPassword = remember { mutableStateOf(false) }
 
+
+    Image(
+        painter = painterResource(R.drawable.huella2), // Imagen de huella
+        contentDescription = "Huella",
+        modifier = Modifier
+            .size(150.dp)
+            .offset(x = (-100).dp, y = (-300).dp) // Posición superior izquierda
+    )
+    Image(
+        painter = painterResource(R.drawable.huella2),
+        contentDescription = "Huella",
+        modifier = Modifier
+            .size(170.dp)
+            .offset(x = (50).dp, y = (300).dp) // Posición inferior derecha
+    )
+
+
     Column (
-        modifier = Modifier.fillMaxSize().padding(16.dp),
+        modifier = Modifier.fillMaxSize().padding(16.dp).background(Color(0xFF00CDD7)),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     )
