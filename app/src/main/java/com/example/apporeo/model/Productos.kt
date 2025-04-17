39ff765cb41376c4113e05c88e8f5f675481ced3
Alvaro5-fm <floresmore1507@gmail.com>
2025-04-17 10:14:23 -0500

    Subiendo modelos

--- app/src/main/java/com/example/apporeo/model/Productos.kt
+++ app/src/main/java/com/example/apporeo/model/Productos.kt
@@ -1,5 +1,13 @@
 package com.example.apporeo.model
 
+import java.util.Date
+
 data class Productos(
+
+    val codigo: String,
+    val nombreProducto: String,
+    val descripcion: String,
+    val cantidad: Int,
+    val fechaVencimiento: Date
 
 )
